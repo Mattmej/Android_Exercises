a933c6bf697a213e95afa0b6e9cba7d51ed8dc3b
Matt <mattcmej@gmail.com>
2018-11-05 00:00:50 -0500

    Change HelloConstraint to use view.setBackgroundColor(), and finish questions for 1.2 part b.

--- Lesson_1/1.2/HelloConstraint/app/src/main/java/com/example/matt/helloconstraint/MainActivity.java
+++ Lesson_1/1.2/HelloConstraint/app/src/main/java/com/example/matt/helloconstraint/MainActivity.java
@@ -12,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
-    private Button isNotZero;
+    private Button zeroButton;
+    private Button countButton;
 
 
     @Override
@@ -31,16 +32,20 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
-            isNotZero = (Button) findViewById(R.id.button_zero);
+            zeroButton = (Button) findViewById(R.id.button_zero);
+            zeroButton.setBackgroundColor(Color.RED);
 
             if (mCount % 2 == 0) {  // If mCount is even
-                isNotZero.setBackgroundColor(Color.CYAN);
-                isNotZero.setTextColor(Color.BLACK);
+//                isNotZero.setBackgroundColor(Color.CYAN);
+//                isNotZero.setTextColor(Color.BLACK);
+                view.setBackgroundColor(Color.CYAN);
+//                view.setTextColor(Color.BLACK);
             }
 
             else {                  // if mCount is odd
-                isNotZero.setBackgroundColor(Color.RED);
-                isNotZero.setTextColor(Color.WHITE);
+//                isNotZero.setBackgroundColor(Color.RED);
+//                isNotZero.setTextColor(Color.WHITE);
+                view.setBackgroundColor(Color.GREEN);
             }
         }
     }
@@ -48,7 +53,10 @@ public class MainActivity extends AppCompatActivity {
     public void setToZero(View view) {
         mCount = 0;
         mShowCount.setText(Integer.toString(mCount));
-        isNotZero.setBackgroundColor(Color.DKGRAY);
-        isNotZero.setTextColor(Color.WHITE);
+//        isNotZero.setBackgroundColor(Color.DKGRAY);
+        view.setBackgroundColor(Color.DKGRAY);
+//        isNotZero.setTextColor(Color.WHITE);
+        countButton = (Button) findViewById(R.id.button_count);
+        countButton.setBackgroundColor(Color.parseColor("#008577"));
     }
 }
